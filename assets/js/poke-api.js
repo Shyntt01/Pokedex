c3816b288bee5ad96bf1862711d20fcceaac28e9
felipe <felipe.impt@hotmail.com>
2023-08-22 21:07:08 -0300

    Pokedex

---
+++ assets/js/poke-api.js
@@ -0,0 +1,34 @@
+const pokeApi = {};
+
+function convertPokeApiDetailToPokemon(pokeDetail, index) {
+    const pokemon = new Pokemon();
+    pokemon.number = index + 1; 
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.photo = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.number}.gif`;
+
+    return pokemon;
+}
+
+pokeApi.getPokemonDetail = (pokemon, index) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then((pokeDetail) => convertPokeApiDetailToPokemon(pokeDetail, index));
+};
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => {
+            return Promise.all(pokemons.map((pokemon, index) => pokeApi.getPokemonDetail(pokemon, index)));
+        });
+};
